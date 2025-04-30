44c92bd74aad1a2ff7b0ae8dad28201743885cf9
Fanykyta <sthefany.bottcher@hotmail.com>
2025-04-29 21:51:14 -0300

    subindo os arquivos

---
+++ Back-end/PessoasApi/Controllers/PessoasController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PessoasApi.Models;
+using Bogus;
+using Bogus.Extensions.Brazil; 
+
+namespace PessoasApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class PessoasController : ControllerBase
+    {
+        private static List<Pessoa>? _pessoasMockadas; 
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Pessoa>> GetPessoas()
+        {
+            if (_pessoasMockadas == null)
+            {
+                _pessoasMockadas = GerarPessoasMockadas(30);
+            }
+            return Ok(_pessoasMockadas);
+        }
+
+        private List<Pessoa> GerarPessoasMockadas(int quantidade)
+        {
+            var faker = new Faker<Pessoa>("pt_BR")
+                .RuleFor(p => p.Cpf, f => f.Person.Cpf(false)) 
+                .RuleFor(p => p.Nome, f => f.Name.FullName())
+                .RuleFor(p => p.Genero, f => f.PickRandom("Masculino", "Feminino", "Outro"))
+                .RuleFor(p => p.Idade, f => f.Random.Number(18, 90))
+                .RuleFor(p => p.Endereco, f => f.Address.StreetAddress())
+                .RuleFor(p => p.Municipio, f => f.Address.City())
+                .RuleFor(p => p.Estado, f => f.Address.StateAbbr());
+
+            return faker.Generate(quantidade);
+        }
+    }
+}
---
+++ Back-end/PessoasApi/Program.cs
@@ -0,0 +1,35 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseCors(MyAllowSpecificOrigins); 
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
